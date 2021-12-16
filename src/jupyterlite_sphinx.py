776d7ff191adb32316dca784a1820635287609fb
martinRenou <martin.renou@gmail.com>
2021-12-16 16:21:37 +0100

    Fix output dir

--- src/jupyterlite_sphinx.py
+++ src/jupyterlite_sphinx.py
@@ -6,7 +6,7 @@ from sphinx.application import Sphinx
 
 def jupyterlite_build(app: Sphinx, error):
     print("[jupyterlite-sphinx] Running JupyterLite build")
-    subprocess.call(["jupyter", "lite", "build", app.srcdir, app.outdir])
+    subprocess.call(["jupyter", "lite", "build", app.srcdir, "--output-dir", app.outdir])
 
 
 def setup(app):
