6ccb288ca1aefe8d5822592c23c56d06762796a5
martinRenou <martin.renou@gmail.com>
2023-02-03 10:00:59 +0100

    Release 0.7.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='jupyterlite-sphinx',
-    version='0.7.2',
+    version='0.7.3',
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
