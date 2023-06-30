24a00cf78b398b4014a7e532665a860d6caab257
martinRenou <martin.renou@gmail.com>
2023-06-30 11:23:59 +0200

    Fix notebook path for voici

--- jupyterlite_sphinx/jupyterlite_sphinx.py
+++ jupyterlite_sphinx/jupyterlite_sphinx.py
@@ -163,7 +163,7 @@ class VoiciIframe(_PromptedIframe):
         lite_options={},
         **attributes,
     ):
-        app_path = f"voici/render/{notebook}"
+        app_path = f"voici/render/{notebook.replace(".ipynb", ".html")}"
 
         options = "&".join(
             [f"{key}={quote(value)}" for key, value in lite_options.items()]
