3e799aafccb9c1cc59616fd7704b5da16ff53756
Justus Magin <keewis@posteo.de>
2025-08-24 21:41:01 +0200

    run `black`

--- jupyterlite_sphinx/jupyterlite_sphinx.py
+++ jupyterlite_sphinx/jupyterlite_sphinx.py
@@ -22,7 +22,11 @@ from sphinx.util.docutils import SphinxDirective
 from sphinx.util.fileutil import copy_asset
 from sphinx.parsers import RSTParser
 
-from ._try_examples import examples_to_notebook, insert_try_examples_directive, new_code_cell
+from ._try_examples import (
+    examples_to_notebook,
+    insert_try_examples_directive,
+    new_code_cell,
+)
 
 import jupytext
 import nbformat
