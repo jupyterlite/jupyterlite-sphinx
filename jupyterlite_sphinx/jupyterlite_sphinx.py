662f3f71f1e7aa3c5a95405e968e8cf82f2af331
Justus Magin <keewis@users.noreply.github.com>
2025-10-20 09:15:09 +0200

    allow adding a preamble for `try_examples` (#293)
    * optionally allow adding a preamble
    
    * import `new_code_cell`
    
    * run `black`
    
    * use a configuration option of type `str` instead
    
    * document the new setting

--- jupyterlite_sphinx/jupyterlite_sphinx.py
+++ jupyterlite_sphinx/jupyterlite_sphinx.py
@@ -22,7 +22,11 @@ from sphinx.util.docutils import SphinxDirective
 from sphinx.util.fileutil import copy_asset
 from sphinx.parsers import RSTParser
 
-from ._try_examples import examples_to_notebook, insert_try_examples_directive
+from ._try_examples import (
+    examples_to_notebook,
+    insert_try_examples_directive,
+    new_code_cell,
+)
 
 import jupytext
 import nbformat
@@ -823,6 +827,12 @@ class TryExamplesDirective(SphinxDirective):
 
         if notebook_unique_name is None:
             nb = examples_to_notebook(self.content, warning_text=warning_text)
+
+            preamble = self.env.config.try_examples_preamble
+            if preamble:
+                # insert after the "experimental" warning
+                nb.cells.insert(1, new_code_cell(preamble))
+
             self.content = None
             notebooks_dir = Path(self.env.app.srcdir) / CONTENT_DIR
             notebook_unique_name = f"{uuid4()}.ipynb".replace("-", "_")
@@ -1151,6 +1161,7 @@ def setup(app):
         default=None,
         rebuild="html",
     )
+    app.add_config_value("try_examples_preamble", default=None, rebuild="html")
 
     # Allow customising the button text for each directive (this is useful
     # only when "new_tab" is set to True)
